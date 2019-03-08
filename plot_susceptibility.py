5736f3941004a0710505af89898d8da9cd9c9806
gbalduzz <gbalduzz@itp.phys.ethz.ch>
2019-03-08 15:11:31 +0100

    added magnetic suscpeptibility script

---
+++ plot_susceptibility.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+from matplotlib import pyplot as plt
+import numpy as np
+import json
+
+file = json.load(open("input.json"))
+
+betas = 1./np.array(file["Ts"])
+Ls = file["Ls"]
+
+def jackKnife(procedure, x, n):
+    block_size = int(np.ceil(len(x) / n))
+    f = []
+    for i in range(n):
+        start = int(i*block_size)
+        end = int(min((i+1)*block_size, len(x)-1))
+        x_loo = np.concatenate([x[:start], x[end:]])
+        f.append(procedure(x_loo))
+
+    f_bar = procedure(x)
+    f = np.array(f)
+
+    delta = np.sqrt((n-1)/n * np.sum((f-f_bar)**2))
+    estimate = n * f_bar - (n-1) * np.mean(f)
+    return estimate, delta
+
+def sort(array, col):
+    return array[array[:,col].argsort()]
+
+for L in Ls:
+    data = np.loadtxt("magnetization_L"+str(L)+".txt")
+    betas = data[:, 0]
+    out = []
+
+    for id, beta in enumerate(betas):
+
+        M = data[id, 1:]
+
+        def susceptibility(m) :
+            m2 = (m**2).mean()
+            m = m.mean()
+            return beta*(m2-m**2)
+
+        val, err = jackKnife(susceptibility, M, 20)
+        out.append([1./beta, val, err])
+
+    out = sort(np.array(out), 0)
+    plt.errorbar(out[:,0], out[:,1], fmt = "--o", yerr = out[:,2], label = "L="+str(L))
+
+plt.legend(loc="best")
+plt.xlabel(r"$T$")
+plt.ylabel(r"$\chi$")
+plt.title("Magnetic susceptibility.")
+plt.show()
